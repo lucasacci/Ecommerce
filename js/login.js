4fa16d17889135fd9b435f56c1f1380084df6a9f
Santiago <santiagof00@gmail.com>
2022-09-13 18:18:25 -0300

    Agrego funcionalidad de login

--- js/login.js
+++ js/login.js
@@ -1,20 +1,55 @@
-document.querySelector('#iniciarsesion').addEventListener('click',iniciar);
+document.querySelector('#btnLogin').addEventListener("click", iniciarSesion);
+document.querySelector('#formInicio').addEventListener('submit',iniciar);
+let isAdmin = false;
 
-function iniciar(){
-    let nuevaPersona = [],
-     usuario ='',
-     contraseña = '';
+const modalLogin = new bootstrap.Modal(
+    document.querySelector("#modalMedialuna")
+  );
 
-     usuario = document.querySelector('#nombreApellido').value;
-     contraseña = document.querySelector('#password').value;
-
-     console.log('Usuario '+ usuario);
-    console.log('Contraseña '+ contraseña);
-     
-    /* nuevaPersona.push(usuario,contraseña)
-     listaAdministradores (nuevaPersona);*/
+function iniciarSesion(){
+    limpiarFormulario();
+    modalLogin.show();
 }
 
-/*function mosrtrarDatos(){
-    let personas = getlistaAdministradores()
-}*/
+function limpiarFormulario() {
+    formInicio.reset();
+  }
+
+function iniciar(e){
+    e.preventDefault();
+
+    usuario = document.querySelector('#usuario').value;
+    contraseña = document.querySelector('#contraseña').value;
+
+    if(usuario === 'usuario' && contraseña === 'usuario'){
+        isAdmin = false;
+        modalLogin.hide();
+    }else if(usuario === 'admin' && contraseña === 'admin'){
+        isAdmin = true;
+        modalLogin.hide();
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Usuario o contraseña incorrecto',
+          })
+    }
+
+    if(isAdmin){
+        let listaNav = document.querySelector('#listaNav');
+        console.log(listaNav.children[1]);
+        console.log(listaNav.firstChild);
+        let insertar = document.createElement('li');
+        insertar.innerHTML = `<a class="nav-link" href="pages/admin.html">Administracion</a>`;
+        insertar.className = 'nav-item';
+        listaNav.insertBefore(insertar,listaNav.children[1]);
+    //     let insertar = `<li class="nav-item">
+    //     <a class="nav-link" href="pages/admin.html">Administracion</a>
+    //   </li>`;
+    //     listaNav.insertBefore(insertar,listaNav[1])
+    
+    }
+    
+    console.log(isAdmin)
+
+}
