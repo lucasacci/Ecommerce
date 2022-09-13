a98bde45d5529f6d588e4f08b7fa6af12bad0892
Luciano <lucasacci@hotmail.com>
2022-09-12 22:32:30 -0300

    Nuevas card

--- js/mainPage.js
+++ js/mainPage.js
@@ -18,34 +18,25 @@ function crearColumna(el){
 
 
     padre.innerHTML += `
-    <div class="wrapper">
-    <div class="container">
-      <div class="top">${el.imagen}</div>
-      <div class="bottom">
-        <div class="left">
-          <div class="details">
-            <h1>${el.nombre}</h1>
-            <p>${el.precio}</p>
-          </div>
-          <div class="buy"><i class="fa-solid fa-cart-plus"></i></div>
-        </div>
-        <div class="right">
-          <div class="done"><i class="material-icons">done</i></div>
-          <div class="details">
-            <h1>${el.nombre}</h1>
-            <p>Added to your cart</p>
-          </div>
-          <div class="remove"><i class="material-icons">clear</i></div>
-        </div>
-      </div>
-    </div>
-    <div class="inside">
-      <div class="icon"><i class="fa-solid fa-circle-info fs-3"></i></div>
-      <div class="contents">
-        </table>
-      </div>
-    </div>
-  </div>`;
+    <div class="col-md-3" id="seccionPadre">
+                  <div class="wsk-cp-product">
+                    <div class="wsk-cp-img">
+                      <img src="${el.imagen}" alt="Product" class="img-responsive" />
+                    </div>
+                    <div class="wsk-cp-text">
+                      <div class="category">
+                        <span>${el.categoria}</span>
+                      </div>
+                      <div class="title-product">
+                        <h3>${el.title}</h3>
+                      </div>
+                      <div class="card-footer">
+                        <div class="wcf-left"><span class="price">$${el.precio}</span></div>
+                        <div class="wcf-right"><a href="#" class="buy-btn"><i class="fa-solid fa-info"></i></a></div>
+                        <div class="wcf-right mx-2"><a href="#" class="buy-btn"><i class="fa-regular fa-heart"></i></a></div>
+                      </div>
+                    </div>
+                  </div>`;
 
 }
 
